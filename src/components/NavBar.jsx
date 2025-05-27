130153615492b31a1a2a14522316758b853228f9
Isaac Fuentes <ibfuentesm@gmail.com>
2025-05-27 06:20:36 -0700

    Create dropdown menu in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import logo from '../assets/logoFuga.png'
 import search from '../assets/magnifyingGlassIcon.svg'
+import { ChevronRight } from 'lucide-react'
 
 function NavBar() {
     return (
@@ -8,11 +9,27 @@ function NavBar() {
             <div className='flex items-center'>
                 <img src={ logo } alt="Logo" className='h-full rounded-sm'/>
             </div>
-            <div className='flex justify-around gap-3 *:px-3 *:py-2.5 *:m-auto'>
-                <Link>Productos</Link>
-                <Link>Novedades</Link>
-                <Link>Distribuidores</Link>
-                <Link>Contacto</Link>
+            <div className='flex justify-around gap-3 *:m-auto '>
+                <div class='dropdown'>
+                    <button class='dropdown-btn'>
+                        <span>Categorias</span>
+                        <ChevronRight class='dropdown-icon' />
+                    </button>
+                    <div class='dropdown-content'>
+                        <a href="">Pegamentos</a>
+                        <a href="">Pigmentos</a>
+                        <a href="">Lavados</a>
+                        <a href="">Herramientas</a>
+                        <a href="">Thinners</a>
+                        <a href="">Lacas</a>
+                        <a href="">Pinturas</a>
+                        <a href="">Limpiador de plástico</a>
+                        <a href="">Oxidante de orugas</a>
+                    </div>
+                </div>
+                <a className='px-6 py-3'>Novedades</a>
+                <a className='px-6 py-3'>Distribuidores</a>
+                <a className='px-6 py-3'>Contacto</a>
             </div>
             <div className='p-5'>
                 <img src={search} alt='Search icon' className='h-full'/>
